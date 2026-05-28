44a50fe4a4447f316bda332a660a5ee9fd7f823d
MJ Maca <mjanemaca@gmail.com>
2026-05-28 20:24:47 +0800

    feat: Added GridCell component as the game button with player image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+"use client";
+
 import { FC } from "react";
+import GridCell from "../components/GridCell";
 
 const Home: FC = () => {
-  return <div className="m-auto">POKEMON TIC TAC TOE</div>;
+  return (
+    <div className="flex flex-col gap-4 m-auto">
+      <span>POKEMON TIC TAC TOE COMPONENTS</span>
+      <div className="flex gap-4">
+        <GridCell index={0} value="X" onClick={() => console.log("clicked")} />
+        <GridCell index={1} value="Y" onClick={() => console.log("clicked")} />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
