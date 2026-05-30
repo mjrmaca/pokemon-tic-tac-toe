577720bd783d19ae7ab65684021e4c7896243473
MJ Maca <mjanemaca@gmail.com>
2026-05-30 19:07:19 +0800

    feat: Created route handler for battle, collection, and history and display under development

---
+++ src/app/collection/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { FC } from "react";
+import UnderDevelopment from "@/src/error-pages/UnderDevelopment";
+
+const Page: FC = () => {
+  return <UnderDevelopment />;
+};
+
+export default Page;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,30 +2,16 @@
 
 import { FC } from "react";
 import GridCell from "../components/GridCell";
-import AppHeader from "../components/AppHeader";
-import AppFooter from "../components/AppFooter";
 
 const Home: FC = () => {
   return (
-    <>
-      <AppHeader />
-      <div className="flex flex-col gap-4 m-auto">
-        <span>POKEMON TIC TAC TOE COMPONENTS</span>
-        <div className="flex gap-4">
-          <GridCell
-            index={0}
-            value="X"
-            onClick={() => console.log("clicked")}
-          />
-          <GridCell
-            index={1}
-            value="Y"
-            onClick={() => console.log("clicked")}
-          />
-        </div>
+    <div className="flex flex-col gap-4 m-auto">
+      <span>POKEMON TIC TAC TOE COMPONENTS</span>
+      <div className="flex gap-4">
+        <GridCell index={0} value="X" onClick={() => console.log("clicked")} />
+        <GridCell index={1} value="Y" onClick={() => console.log("clicked")} />
       </div>
-      <AppFooter />
-    </>
+    </div>
   );
 };
 
