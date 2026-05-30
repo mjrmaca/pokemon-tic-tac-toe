1b036c98e6287b9f56108c43e039670f2aafee1d
MJ Maca <mjanemaca@gmail.com>
2026-05-30 18:14:08 +0800

    feat: Adjusted navigation to include ui for mobile view

--- src/components/AppHeader/index.tsx
+++ src/components/AppHeader/index.tsx
@@ -1,20 +1,15 @@
 "use client";
 
-import clsx from "clsx";
 import { FC } from "react";
-import Navigation from "./components/Navigation";
+import AppNavigation from "../AppNavigation";
 import Brand from "./components/Brand";
 
 const AppHeader: FC = () => {
   return (
     <header className="bg-surface-bright w-full top-0 sticky border-b-4 border-gray-200 z-40">
-      <div
-        className={clsx([
-          "flex items-center justify-between px-2 md:px-4 py-4 max-w-7xl mx-auto",
-        ])}
-      >
+      <div className="flex items-center justify-between px-2 md:px-4 py-4 max-w-7xl mx-auto">
         <Brand />
-        <Navigation />
+        <AppNavigation type="header" />
       </div>
     </header>
   );
